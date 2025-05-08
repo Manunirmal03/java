026d3b2129ecb1ccb959d798b16b7d4e2adab65b
Manunirmal03 <134664437+Manunirmal03@users.noreply.github.com>
2025-05-08 15:29:26 +0530

    pgm10

---
+++ area.java
@@ -0,0 +1,18 @@
+import graphics.*;
+class area{
+ public static void main(String args[]){
+    rectangle  rt= new rectangle();
+    square sq=new square();
+    circle cr=new circle();
+    triangle tr=new triangle();
+    rt.area();
+    rt.display();
+    sq.area();
+    sq.display();
+    cr.area();
+    cr.display();
+    tr.area();
+    tr.display();
+    
+    }
+   }
